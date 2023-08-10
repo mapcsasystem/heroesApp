40e37a608cc5c7662c6d75f69730bb1798806d94
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-10 16:28:14 -0600

    hero add page

---
+++ src/app/dashboard/pages/hero-add-page/hero-add-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-add-page',
+  templateUrl: './hero-add-page.component.html',
+  styleUrls: ['./hero-add-page.component.scss']
+})
+export class HeroAddPageComponent {
+
+}
