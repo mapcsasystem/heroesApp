43d50bdbe209396750b303a20f8ff8ba41f96190
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-10 16:29:03 -0600

    heroes list page

---
+++ src/app/dashboard/pages/heroes-list-page/heroes-list-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list-page',
+  templateUrl: './heroes-list-page.component.html',
+  styleUrls: ['./heroes-list-page.component.scss']
+})
+export class HeroesListPageComponent {
+
+}
