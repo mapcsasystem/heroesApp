13c5c7d6885e3687f14b43ab513d5bd02f573121
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-12 17:29:35 -0600

    add menu dshboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,18 +2,33 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../auth/services/auth.service';
+import { IMenuItems } from './interfaces/menu.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  private readonly _breakpointObserver = inject(BreakpointObserver);
+  private readonly _authService = inject(AuthService);
+  isHandset$: Observable<boolean> = this._breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
+  menuItems: IMenuItems[] = [
+    {
+      label: 'Listado de héroes',
+      icon: 'label',
+      url: './heroes-list',
+    },
+    { label: 'Añadir héroe', icon: 'add', url: './hero-add' },
+    { label: 'Buscar héroe', icon: 'search', url: './hero-search' },
+  ];
+  logout(): void {
+    this._authService.logout();
+  }
 }
